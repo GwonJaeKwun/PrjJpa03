9c4ef9019b75b9092205922a5711bb8425b895b6
GwonJaeKwun <nannada1590@gmail.com>
2025-09-11 12:45:45 +0900

    PrjJpa03 ver2

--- src/main/java/com/green/dto/Article.java
+++ src/main/java/com/green/dto/Article.java
@@ -34,10 +34,26 @@ public class Article {
 		return "Article [id=" + id + ", title=" + title + ", content=" + content + "]";
 	}
 	
-	// Getter 
+	// Getter  / Setter
 	public Long getId() {		
 		return this.id;
 	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 		
 	
 }
