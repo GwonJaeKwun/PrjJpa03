2d448cba7c47fc31411952b38a5cf4c9fabb6538
GwonJaeKwun <nannada1590@gmail.com>
2025-09-15 12:33:07 +0900

    PrjJpa03 수정0915

--- src/main/java/com/green/api/ArticleApiController.java
+++ src/main/java/com/green/api/ArticleApiController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,15 +24,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class ArticleApiController {
-
+	
 	@Autowired
 	private ArticleService  articleService;
-	
+
 	// 결과 data 로 출력 : JSON 기본값
+	// build.gradle 에 jackson dataformat xml 라이브러리를 추가하면 기본 data 로 출력
+	// 라이브러리를 추가한 후 json을 출력할려면 produces = "application/json;charset=UTF-8"  추가
 	@GetMapping(value="/api/articles",
-			produces = "application/json;charset=UTF-8" )
-			// produces = "application/xml;charset=UTF-8" )			
-	        // produces = MediaType.APPLICATION_JSON_VALUE )
+			produces = "application/json;charset=UTF-8" )  // json 
 	public  List<Article> list() {
 		
 		List<Article>  list = articleService.getList();
@@ -45,10 +48,26 @@ public class ArticleApiController {
 		
 	}
 	
-	// ResponsEntity<Article>
+	// ResponsEntity<Article>  
 	//  Artcile  : DATA
 	//  +상태 코드  ResponseEntity.status( HttpStatus.ok  ) 
 	//             또는 ResponseEntity.status( HttpStatus.BAD_REQUEST  ) 
+	
+	// 한개 조회
+	@GetMapping("/api/articles/{id}" )    // xml 로 출력
+	public ResponseEntity<Article>  getOne(
+		@PathVariable("id")	 Long  id
+			) {   
+		
+		Article  article  =  articleService.getOne( id );   
+		log.info("article:" + article );
+		ResponseEntity<Article> result = 
+			( article != null ) 
+			  ?	 ResponseEntity.status(HttpStatus.OK).body( article )
+			  :  ResponseEntity.status(HttpStatus.BAD_REQUEST).build();		  
+		
+		return  result;
+	} 
 	
 	@PostMapping("/api/articles")
 	public ResponseEntity<Article> create( 
@@ -65,6 +84,42 @@ public class ArticleApiController {
 		
 		return  result;
 	}
+	
+	@DeleteMapping("/api/articles/{id}")
+	public  ResponseEntity<Article>  delete(
+		@PathVariable("id")  Long id		
+			) {
+		
+		Article  deleted  = articleService.delete(id);
+		
+		ResponseEntity<Article>  result = 
+			( deleted != null )	
+				? ResponseEntity.status(HttpStatus.OK).body( deleted )   // 200(ok) , 201(created)	  
+				: ResponseEntity.status(HttpStatus.BAD_REQUEST).build(); // 400	 (Error)	
+
+		return  result;
+		
+	}
+
+	//  수정
+	//  Put   : 전체 데이터를 수정 -> 모든데이터가 필여 
+	//  Patch : 전체, 일부 데이터를 수정 
+	// @RequestBody : 넘어오는 정보는 json 구조이다
+	@PatchMapping("/api/articles")
+	public  ResponseEntity<Article>  update(
+		@RequestBody  Article    article	        // <- JSON.string
+			) {
+		
+		Article updated =  articleService.update( article );
+		ResponseEntity<Article>  result =
+			(updated != null) 
+			? ResponseEntity.status(HttpStatus.OK).body( updated )
+			: ResponseEntity.status(HttpStatus.BAD_REQUEST).build();  // 400
+					
+		return result;
+		
+	}
+	
 	
 }
 
--- src/main/java/com/green/service/ArticleService.java
+++ src/main/java/com/green/service/ArticleService.java
@@ -9,6 +9,9 @@ import com.green.dto.Article;
 import com.green.dto.ArticleDto;
 import com.green.repository.ArticleRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class ArticleService {
 	
@@ -32,6 +35,47 @@ public class ArticleService {
 		Article   saved    =  articleRepository.save( article );
 		
 		return  saved; 
+	}
+
+	public Article getOne(Long id) {
+		
+		// db id 로 조회
+	    Article  article  = articleRepository.findById(id).orElse(null);
+		log.info(  "article:" + article ); 
+		return   article;
+	}
+
+	public Article delete(Long id) {
+		
+		// 삭재할 때는 미리 검색을 하고 cache memory 에서
+		Article   target  =  articleRepository.findById(id).orElse(null);
+		
+		if(target == null)
+			return  null;
+		
+		articleRepository.delete( target );
+		
+		return target;
+	}
+
+	public Article update(Article article) {
+		
+		// 0. 수정할 자료의 id 
+		Long     id      =  article.getId();
+		
+		// 1. 수정할 데이터를 검색
+		Article  target  =  articleRepository.findById(id).orElse(null);
+		
+		// 2. target 이 null 이면
+		if( target == null ||  target.getId() != id  ) {
+			log.info("id:{} article:{}", id, article );
+			return null;  // 잘못된 요청 400
+		}
+		
+		// 3. 업데이트 ( JPA -> .save())
+		Article updated = articleRepository.save( article );
+		
+		return  updated;
 	}
 	
 }
